3b9c804d911becf032d5c28aae06a37f0cd77ffc
devduck <tommyho@cpp.edu>
2021-09-10 17:34:24 -0700

    working SPA bare features

--- main.js
+++ main.js
@@ -1,29 +1,26 @@
 function showPage(page) {
-    document.querySelectorAll('section.page').forEach(section => {
-        section.style.display = 'none';
-    });
+  document.querySelectorAll("section.page").forEach((section) => {
+    section.style.display = "none";
+  });
 
-    let currPageID = `#${page}`;
+  let currPageID = `#${page}`;
 
-    console.log(currPageID); // returns id #pageN
+  console.log(currPageID); // returns id #pageN
 
-    if (currPageID === '#page2') {
-        document.querySelector('#intro').style.display = 'block';
-    }
-    // FIXME: page3 does NOT work!!!
-    else if (currPageID === '#page3') {
-        document.querySelector('#features-and-pricing').style.display = 'flex';
-    }
-    else if (currPageID === '#page4') {
-        document.querySelector('#contact').style.display = 'block';
-    }
+  if (currPageID === "#page2") {
+    document.querySelector("#intro").style.display = "block";
+  } else if (currPageID === "#page3") {
+    document.querySelector("#features-and-pricing").style.display = "flex";
+  } else if (currPageID === "#page4") {
+    document.querySelector("#contact").style.display = "block";
+  }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('button').forEach(button => {
-        button.onclick = function() {
-            // console.log(this.dataset.page);
-            showPage(this.dataset.page);
-        }
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("button").forEach((button) => {
+    button.onclick = function () {
+      // console.log(this.dataset.page);
+      showPage(this.dataset.page);
+    };
+  });
 });
