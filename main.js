d1cd53e97ce719d3d71977e83a639d6a605da828
tommy <tommyho@cpp.edu>
2021-09-24 17:30:47 -0700

    contact-us working

--- main.js
+++ main.js
@@ -17,10 +17,52 @@ function showPage(page) {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
+  // control nav buttons
   document.querySelectorAll("button").forEach((button) => {
     button.onclick = function () {
       // console.log(this.dataset.page);
       showPage(this.dataset.page);
     };
   });
+
+  // validate per first-name input event
+  document.querySelector('#first-name').onkeyup = () => {
+    if (document.querySelector('#first-name').value.length > 0 && document.querySelector('#last-name').value.length > 0 && document.querySelector('#email').value.length > 0 && document.querySelector('#comments').value.length > 0) {
+      document.querySelector('#contact-btn').disabled = false;
+    }
+    else {
+      document.querySelector('#contact-btn').disabled = true;
+    }
+  }
+
+  // validate per last-name input event
+  document.querySelector('#last-name').onkeyup = () => {
+    if (document.querySelector('#first-name').value.length > 0 && document.querySelector('#last-name').value.length > 0 && document.querySelector('#email').value.length > 0 && document.querySelector('#comments').value.length > 0) {
+      document.querySelector('#contact-btn').disabled = false;
+    }
+    else {
+      document.querySelector('#contact-btn').disabled = true;
+    }
+  }
+
+  // validate per email input event
+  document.querySelector('#email').onkeyup = () => {
+    if (document.querySelector('#first-name').value.length > 0 && document.querySelector('#last-name').value.length > 0 && document.querySelector('#email').value.length > 0 && document.querySelector('#comments').value.length > 0) {
+      document.querySelector('#contact-btn').disabled = false;
+    }
+    else {
+      document.querySelector('#contact-btn').disabled = true;
+    }
+  }
+
+  // validate per comments input event
+  document.querySelector('#comments').onkeyup = () => {
+    if (document.querySelector('#first-name').value.length > 0 && document.querySelector('#last-name').value.length > 0 && document.querySelector('#email').value.length > 0 && document.querySelector('#comments').value.length > 0) {
+      document.querySelector('#contact-btn').disabled = false;
+    }
+    else {
+      document.querySelector('#contact-btn').disabled = true;
+    }
+  }
+
 });
